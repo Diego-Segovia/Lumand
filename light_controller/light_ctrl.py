568966406b124f41af7c62401442ce06d2057698
Diego-Segovia <diego.segovia348@gmail.com>
2022-01-10 00:54:55 -0500

    added fast mode

--- light_controller/light_ctrl.py
+++ light_controller/light_ctrl.py
@@ -14,32 +14,36 @@ class LightController():
             print(f'HTTP error occurred: {http_err}')
 
         return response.json().get('results')[0]
+
+    def fast_mode(self):
+        self.payload['fast'] = True
     
-    def turn_on(self):
-        if self.payload.get('power', False):
-            if self.payload['power'] != 'on':
-                self.payload['power'] = 'on'
-        else:
-            self.payload['power'] = 'on'
+    def turn_on(self, go_fast=False):
+        if go_fast:
+            self.fast_mode()
+
+        self.payload['power'] = 'on'
 
         return self.do_action()
         
 
-    def turn_off(self):
-        if self.payload.get('power', False):
-            if self.payload['power'] != 'off':
-                self.payload['power'] = 'off'
-        else:
-            self.payload['power'] = 'off'
+    def turn_off(self, go_fast=False):
+        if go_fast:
+            self.fast_mode()
+            
+        self.payload['power'] = 'off'
 
         return self.do_action()
 
-    def set_brightness(self, val=1.0):
+    def set_brightness(self, val=1.0, go_fast=False):
         if type(val) is not float:
             raise TypeError('Brightness value must be a float.')
 
         if not 0.0 <= val <= 1.0:
             raise ValueError('Brightness value must be between 0.0 and 1.0.')
+        
+        if go_fast:
+            self.fast_mode()
 
         self.payload['brightness'] = val
 
