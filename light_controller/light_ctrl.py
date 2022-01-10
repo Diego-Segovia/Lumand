6a90ec438c16ca8f71e538e9a21a439516a93c4c
Diego-Segovia <diego.segovia348@gmail.com>
2022-01-10 01:03:17 -0500

    Fixed fast mode

--- light_controller/light_ctrl.py
+++ light_controller/light_ctrl.py
@@ -5,7 +5,8 @@ class LightController():
     def __init__(self, token):
         self.headers = {"Authorization": "Bearer %s" % token,}
         self.payload = {}
-    
+
+    # Execute Query
     def do_action(self):
         try:
             response = requests.put('https://api.lifx.com/v1/lights/all/state', data=self.payload, headers=self.headers)
@@ -15,9 +16,11 @@ class LightController():
 
         return response.json().get('results')[0]
 
+    # Enable Fast Mode: Execute the query fast, without initial state checks and wait for no results.
     def fast_mode(self):
         self.payload['fast'] = True
     
+    # Turn light on
     def turn_on(self, go_fast=False):
         if go_fast:
             self.fast_mode()
@@ -26,15 +29,16 @@ class LightController():
 
         return self.do_action()
         
-
+    # Turn light off
     def turn_off(self, go_fast=False):
         if go_fast:
             self.fast_mode()
-            
+
         self.payload['power'] = 'off'
 
         return self.do_action()
 
+    # Set Brightness Level between 0.0 and 1.0
     def set_brightness(self, val=1.0, go_fast=False):
         if type(val) is not float:
             raise TypeError('Brightness value must be a float.')
